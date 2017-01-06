cfd79b88ddff93542c5e7dc9411e77dc5956daf6
samueljklee <samueljklee@gmail.com>
2017-01-05 16:17:37 -0800

    Create TempSensor.ino

---
+++ TempSensor.ino
@@ -0,0 +1,71 @@
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+/* 
+Reference: https://randomnerdtutorials.com/guide-for-ds18b20-temperature-sensor-with-arduino/ 
+http://arduino-info.wikispaces.com/Brick-Temperature-DS18B20#Read%20individual
+
+*/
+
+// Data wire is conntec to the Arduino digital pin 10
+#define ONE_WIRE_BUS 10
+
+// Setup a oneWire instance to communicate with any OneWire devices
+OneWire oneWire(ONE_WIRE_BUS);
+
+// Pass our oneWire reference to Dallas Temperature sensor 
+DallasTemperature sensors(&oneWire);
+
+void setup(void)
+{
+  // Start serial communication for debugging purposes
+  Serial.begin(9600);
+
+  discoverOneWireDevices();  // Reading the address of Temperature Sensor DS18B20
+  
+  // Start up the library
+  sensors.begin();
+}
+
+void loop(void){ 
+  // Call sensors.requestTemperatures() to issue a global temperature and Requests to all devices on the bus
+  sensors.requestTemperatures(); 
+  
+  Serial.print("Celsius temperature: ");
+  // Why "byIndex"? You can have more than one IC on the same bus. 0 refers to the first IC on the wire
+  Serial.print(sensors.getTempCByIndex(0)); 
+  Serial.print(" - Fahrenheit temperature: ");
+  Serial.println(sensors.getTempFByIndex(0));
+  delay(500);
+}
+
+/*-----( Function to get Address of Temperature Sensor DS18B20 )-----*/
+void discoverOneWireDevices(void) {
+  byte i;
+  byte present = 0;
+  byte data[12];
+  byte addr[8];
+
+  Serial.print("Looking for 1-Wire devices...\n\r");// "\n\r" is NewLine 
+  while(oneWire.search(addr)) {
+    Serial.print("\n\r\n\rFound \'1-Wire\' device with address:\n\r");
+    for( i = 0; i < 8; i++) {
+      Serial.print("0x");
+      if (addr[i] < 16) {
+        Serial.print('0');
+      }
+      Serial.print(addr[i], HEX);
+      if (i < 7) {
+        Serial.print(", ");
+      }
+    }
+    if ( OneWire::crc8( addr, 7) != addr[7]) {
+      Serial.print("CRC is not valid!\n\r");
+      return;
+    }
+  }
+  Serial.println();
+  Serial.println("Done");
+  oneWire.reset_search();
+  return;
+}
